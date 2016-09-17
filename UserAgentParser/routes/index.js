b91a986b6b0d18742b7cb42a2f68cf2f6b14d8f7
Sanjid Halim <sanjid.m.halim@vanderbilt.edu>
2016-09-17 16:07:12 -0700

    Added parsing of UA

--- UserAgentParser/routes/index.js
+++ UserAgentParser/routes/index.js
@@ -6,4 +6,21 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'This is a great app' });
 });
 
+router.post('/parseAgent', function(req, res, next){
+	var PythonShell = require('python-shell');
+
+	var options = {
+	  mode: 'text',
+	  pythonPath: 'C:/Python27/python.exe',
+	  pythonOptions: ['-u'],
+	  scriptPath: './',
+	  args: [req.body.userAgent]
+	};
+
+	PythonShell.run('E:/Personal_Projects/understandify/UserAgentParser/routes/parser.py', options, function(err, results){
+		if (err) res.send("This error" + err);
+		else res.send(results);
+	})
+})
+
 module.exports = router;
